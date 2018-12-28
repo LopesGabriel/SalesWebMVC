bdfc2f419bc43ecf45cf35a28d482d7ddbd1fb60
Gabriel de Oliveira Lopes <lopesgabriel0199@gmail.com>
2018-12-28 18:28:30 -0200

    SellerService and basic FindAll

---
+++ SalesWebMvc/Services/SellerService.cs
@@ -0,0 +1,27 @@
+﻿using SalesWebMvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc.Services
+{
+    public class SellerService
+    {
+        private readonly SalesWebMvcContext _context;
+
+        // Construct
+
+        public SellerService(SalesWebMvcContext context)
+        {
+            _context = context;
+        }
+
+        // Methods
+
+        public List<Seller> FindAll()
+        {
+            return _context.Seller.ToList();
+        }
+    }
+}
